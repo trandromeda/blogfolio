c5690b41f45d949ed1840f7721390c04cd9c656e
Andy Tran <transient.tran@gmail.com>
2022-12-05 23:20:09 -0500

    update config with env vars

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,16 +1,29 @@
 import type { GatsbyConfig } from "gatsby";
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: process.env.CONTENTFUL_HOST,
+};
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `My Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
+    },
     options: {
       "accessToken": "dJ19h1i0idbBGVSTRI4luqdwBp1iJk90AmuIjtbkRnE",
       "spaceId": "sl4rlbx6hhhl"
