fa7a03f50b92cbd13dc85229af5a158e9e215cbf
Andy Tran <transient.tran@gmail.com>
2022-12-08 14:08:05 -0500

    lay out pages

---
+++ src/components/layout.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import { container, title, siteTitle } from "./layout.module.css";
+
+type Props = {
+  pageTitle: string;
+  children: React.ReactNode;
+};
+const Layout = ({ pageTitle, children }: Props) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site(siteMetadata: { title: { eq: "My Portfolio" } }) {
+        siteMetadata {
+          description
+          title
+          siteUrl
+        }
+      }
+    }
+  `);
+  return (
+    <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={title}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
