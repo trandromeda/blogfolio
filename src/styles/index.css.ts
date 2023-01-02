d56c749be2d94c1b7a62da651a3fba58e86f358b
Andy Tran <transient.tran@gmail.com>
2023-01-02 11:26:48 -0500

    Feature/Add basic styling to nav (#4)

--- src/styles/index.css.ts
+++ src/styles/index.css.ts
@@ -5,7 +5,6 @@ import {
   createTheme,
 } from "@vanilla-extract/css";
 
-export const colorVar = createVar();
 export const primaryColour = createVar();
 export const backgroundColour = createVar();
 export const accentColour = createVar();
@@ -20,6 +19,22 @@ export const [themeClass, vars] = createTheme({
   },
 });
 
+// TODO: Define global vars in createGlobalTheme()
+globalStyle("html, body", {
+  vars: {
+    [primaryColour]: "#8F3F71",
+    [accentColour]: "#076678",
+    [backgroundColour]: "#F9F5D7",
+  },
+  backgroundColor: backgroundColour,
+  margin: 0,
+});
+
+globalStyle(`li > a`, {
+  textDecoration: "none",
+  color: "black",
+});
+
 export const titleStyle = style({
   color: primaryColour,
   fontWeight: 700,
@@ -27,7 +42,6 @@ export const titleStyle = style({
 });
 
 export const bodyStyle = style({
-  color: accentColour,
   fontSize: 24,
   fontWeight: 700,
 });
@@ -42,15 +56,4 @@ export const asideStyle = style({
   marginTop: 56,
   maxWidth: "50%",
   marginLeft: "auto",
-});
-
-// TODO: Define global vars in createGlobalTheme()
-globalStyle("html, body", {
-  vars: {
-    [primaryColour]: "#8F3F71",
-    [accentColour]: "#076678",
-    [backgroundColour]: "#F9F5D7",
-  },
-  backgroundColor: backgroundColour,
-  margin: 0,
 });
